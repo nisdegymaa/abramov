08dcf48091a57a9681fe8b11b99368f79acc4c97
Mc <c23io1066@ufe.edu.mn>
2024-09-12 17:43:45 +0800

    fix error

--- ABR0035A.cpp
+++ ABR0035A.cpp
@@ -4,23 +4,22 @@
 using namespace std;
 
 int main() {
-    double x, y, z, result1, result2, max;
+    float x, y, z, result1, result2, max;
     cin >> x >> y >> z;
 
     result1 = (x + y + z);
     result2 = (x * y * z);
 
-    if(result1 > result2) {
+    if (result1 > result2) {
         max = result1;
-    }
-    else {
-        return result2;
+    } else {
+        max = result2;
     }
 
     cout.setf(ios::fixed|ios::showpoint);
     cout.precision(1);
 
-    cout << max;
+    cout << max << endl;
+
     return 0;
 }
-a
